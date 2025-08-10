146180f3ab41f97333865bdf804622b2339995be
Alexander Saul <alexandersaul2604@gmail.com>
2025-08-10 17:03:34 -0500

    feat(persona): enhance Persona entity with additional fields for user details
    - Added nullable fields for primerNombre, segundoNombre, primerApellido, segundoApellido, fechaNacimiento, telefono, dni, and createdAt to the Persona entity.
    - Updated import statements to include Column decorator for new fields.

--- src/modules/users/entities/persona.entity.ts
+++ src/modules/users/entities/persona.entity.ts
@@ -1,15 +1,23 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Persona {
     @PrimaryGeneratedColumn()
     id: number;
+    @Column({nullable: true})
     primerNombre: string;
+    @Column({nullable: true})
     segundoNombre: string;
+    @Column({nullable: true})
     primerApellido: string;
+    @Column({nullable: true})
     segundoApellido: string;
+    @Column({nullable: true})
     fechaNacimiento: Date;
+    @Column({nullable: true})
     telefono: string;
+    @Column({nullable: true})
     dni: string;
+    @Column({nullable: true})
     createdAt : Date;
 }
