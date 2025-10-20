8cf14fa7cfee9fa7c42170e55d4483e276347c5f
Luis Javier Castillo <t1053300321@unitru.edu.pe>
2025-10-20 18:43:52 -0500

    asdas

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger.config';
 import { addTransactionalDataSource, initializeTransactionalContext } from 'typeorm-transactional';
